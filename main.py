8a2d4d474740532413860be821db90b6f11743fd
luthfiano <103997569+luthfiano@users.noreply.github.com>
2022-04-19 17:01:37 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Nama : Luthfiano Alfarisi
+# NIM : 191011400437
+# Kelas : 06TPLM003
+
+print ("Tutorial penggunaan looping","\n")
+ 
+count = 0
+while (count < 9):
+    print ("The count is: ", count)
+    count = count + 1
+
+print ("Good bye!")
